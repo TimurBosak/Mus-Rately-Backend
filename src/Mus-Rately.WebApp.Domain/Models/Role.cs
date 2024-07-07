26e848f86e946e42f862a401d7e1e48fb8c41df2
TimurBosak <roadto5knet@gmail.com>
2024-07-08 02:03:44 +0300

    Finish domain models with adding rating, role, user, userrole, usersongrating classes, needed migration for them

---
+++ src/Mus-Rately.WebApp.Domain/Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Mus_Rately.WebApp.Domain.Models
+{
+    public class Role : IdentityRole
+    {
+        public const string User = "User";
+        public const string Artist = "Artist";
+        public const string SiteOwner = "SiteOwner";
+        public const string Admin = "Admin";
+
+
+        public ICollection<UserRole> Users { get; set; }
+    }
+}
--- src/Mus-Rately.WebApp.Repositories/MusRatelyContext.cs
+++ src/Mus-Rately.WebApp.Repositories/MusRatelyContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.EntityFrameworkCore;
 using Mus_Rately.WebApp.Domain.Models;
 
 namespace Mus_Rately.WebApp.Repositories
@@ -46,6 +47,68 @@ namespace Mus_Rately.WebApp.Repositories
                 .HasIndex(sa => sa.SongId);
             modelBuilder.Entity<SongArtist>()
                 .HasIndex(sa => sa.ArtistId);
+
+            modelBuilder.Entity<Rating>();
+
+            modelBuilder.Entity<UserSongRating>()
+                .HasKey(usr => new { usr.UserId, usr.SongId, usr.RatingId });
+            modelBuilder.Entity<UserSongRating>()
+                .HasIndex(usr => usr.UserId);
+            modelBuilder.Entity<UserSongRating>()
+                .HasIndex(usr => usr.SongId);
+            modelBuilder.Entity<UserSongRating>()
+                .HasIndex(usr => usr.RatingId);
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.Name)
+                .HasMaxLength(User.MaxLength)
+                .IsRequired();
+            modelBuilder.Entity<User>()
+                .Property(u => u.UserName)
+                .IsRequired();
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.UserName)
+                .IsUnique();
+            modelBuilder.Entity<User>()
+                .Property(u => u.Email)
+                .IsRequired();
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+            modelBuilder.Entity<User>()
+                .Property(u => u.PasswordHash)
+                .IsRequired();
+
+            modelBuilder.Entity<IdentityUserLogin<string>>().HasKey(l => l.UserId);
+
+            modelBuilder.Entity<UserRole>()
+                .HasKey(ur => new { ur.RoleId, ur.UserId });
+            modelBuilder.Entity<UserRole>()
+                .HasIndex(ur => ur.RoleId);
+            modelBuilder.Entity<UserRole>()
+                .HasIndex(ur => ur.UserId);
+
+            modelBuilder.Entity<Role>().HasData(
+                new Role
+                {
+                    Id = "User",
+                    Name = Role.User,
+                },
+                new Role
+                {
+                    Id = "Artist",
+                    Name = Role.Artist,
+                },
+                new Role
+                {
+                    Id = "SiteOwner",
+                    Name = Role.SiteOwner,
+                },
+                new Role
+                {
+                    Id = "Admin",
+                    Name = Role.Admin,
+                });
         }
     }
 }
