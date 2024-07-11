d9c6715e147ae1d0582a4f4d26533970180b3d17
TimurBosak <roadto5knet@gmail.com>
2024-07-11 17:22:21 +0300

    implement rolestore body

--- src/Mus-Rately.WebApp.Services/Authentication/RoleStore.cs
+++ src/Mus-Rately.WebApp.Services/Authentication/RoleStore.cs
@@ -14,57 +14,84 @@ namespace Mus_Rately.WebApp.Services.Authentication
             _uow = uow;
         }
 
-        public Task<IdentityResult> CreateAsync(Role role, CancellationToken cancellationToken)
+        public async Task<IdentityResult> CreateAsync(Role role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var roleRepository = _uow.GetRepository<Role>();
+
+            roleRepository.Add(role);
+            await _uow.SaveChangesAsync();
+
+            return IdentityResult.Success;
         }
 
-        public Task<IdentityResult> DeleteAsync(Role role, CancellationToken cancellationToken)
+        public async Task<IdentityResult> UpdateAsync(Role role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var roleRepository = _uow.GetRepository<Role>();
+
+            roleRepository.Update(role);
+            await _uow.SaveChangesAsync();
+
+            return IdentityResult.Success;
         }
 
-        public void Dispose()
+        public async Task<IdentityResult> DeleteAsync(Role role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var roleRepository = _uow.GetRepository<Role>();
+
+            roleRepository.Remove(role);
+            await _uow.SaveChangesAsync();
+
+            return IdentityResult.Success;
         }
 
-        public Task<Role> FindByIdAsync(string roleId, CancellationToken cancellationToken)
+        public async Task<Role> FindByIdAsync(string roleId, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var roleRepository = _uow.GetRepository<Role>();
+
+            var role = await roleRepository.GetByIdAsync(roleId);
+
+            return role;
         }
 
-        public Task<Role> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken)
+        public async Task<Role> FindByNameAsync(string normalizedRoleName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var roleRepository = _uow.GetRepository<Role>();
+
+            var role = await roleRepository.GetSingleOrDefaultAsync(r => r.NormalizedName == normalizedRoleName);
+
+            return role;
         }
 
         public Task<string> GetNormalizedRoleNameAsync(Role role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(role.NormalizedName);
         }
 
         public Task<string> GetRoleIdAsync(Role role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(role.Id);
         }
 
         public Task<string> GetRoleNameAsync(Role role, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            return Task.FromResult(role.Name);
         }
 
         public Task SetNormalizedRoleNameAsync(Role role, string normalizedName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            role.NormalizedName = normalizedName;
+
+            return Task.CompletedTask;
         }
 
         public Task SetRoleNameAsync(Role role, string roleName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            role.Name = roleName;
+
+            return Task.CompletedTask;
         }
 
-        public Task<IdentityResult> UpdateAsync(Role role, CancellationToken cancellationToken)
+        public void Dispose()
         {
             throw new NotImplementedException();
         }
